5bc495f8ac0b1f504897a8d8284bb97e055811fd
Ishan Nerlekar <78009247+ishan-nerlekar@users.noreply.github.com>
2026-06-26 00:54:43 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_s=re.sub(r'[^a-zA-Z0-9]','',s).lower().strip()
+        reversed_s=''.join(reversed(cleaned_s))
+        print(cleaned_s)
+        print(reversed_s)
+        return reversed_s==cleaned_s
+        
